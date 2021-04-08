715357600c3e5ff1cd394a272c34e1c9db897346
Vitalii Khanas <vitalikkhanas@ukr.net>
2021-04-09 01:50:06 +0300

    Realized adding tasks, updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
 import Moment from 'moment';
 import {Tasks} from "./components/Tasks";
+import { AddTaskForm } from './components/AddTaskForm';
 
 const initialTasks: Array<Task> = [
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Go online at 19:00", date: Moment(new Date(2021,4,6,17,21)).format('HH:MM DD-MM-YYYY'), completed: true},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
-  {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
   {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
   {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false},
   {text: "Work by progect aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", date: Moment(new Date(2021,4,6,17,15)).format('HH:MM DD-MM-YYYY'), completed: false}
@@ -33,8 +25,14 @@ const App: React.FC = () => {
     setTasks(newTasks);
   };
 
+  const addTask: AddTask = newTask =>{
+    newTask.trim() !== "" && 
+    setTasks([...tasks, {text:newTask, date: Moment(new Date(Date.now())).format('HH:MM DD-MM-YYYY'), completed: false}]);
+  }
+
   return (
     <React.Fragment>
+      <AddTaskForm addTask={addTask}/>
       <Tasks tasks={tasks} toggleTask={toggleTask}/>
     </React.Fragment>
   );
--- src/components/TasksItem.tsx
+++ src/components/TasksItem.tsx
@@ -9,18 +9,21 @@ interface TasksItemProps{
 export const TasksItem: React.FC<TasksItemProps> = ( {task, toggleTask} ) =>{
     return (
         <div className="list_item">
-            <ul>
-            <input type="checkbox" checked={task.completed} onChange={() =>toggleTask(task)}/>
-            <div className="text">
-                <label className={task.completed ? "complete" : undefined}>
-                    {task.text}
-                </label>
-            </div>
-            <div className="date_item">
+            <div className="item-content">
+                <input type="checkbox" className="completed-checkbox" checked={task.completed} onChange={() =>toggleTask(task)}/>
+                <div className="text">
+                    <label className={task.completed ? "complete" : undefined}>
+                        {task.text}
+                    </label>
+                </div>
+                <div className="date_item">
                 <label className="date">Date: </label>
                 <label >{task.date}</label>
+                </div>
             </div>
-            </ul>
+            <div className="del">
+                <button type="button" className="del-button">Delete</button>
+            </div> 
         </div>
     );
 };
