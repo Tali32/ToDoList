721e660486824a8d96f0890c28933397a6aea753
Tali Khanas <47400159+Tali32@users.noreply.github.com>
2021-04-14 13:54:25 +0300

    test commit

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./Footer.css";
-
-export const Footer: React.FC = () =>{
-    return(
-        <p className="footer">Copyright 2021 &copy; Vitalii Khanas</p>
-    );
-};
